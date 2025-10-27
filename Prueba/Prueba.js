44b0002e67232453e10ac2a1abede332d2b2e1f8
Israel Jimenez <israel.jimenez.jimenez.al@iespoligonosur.org>
2025-10-27 08:39:24 +0100

    clase

--- Prueba/Prueba.js
+++ Prueba/Prueba.js
@@ -1,52 +1,20 @@
 
-function pedirNumero(mensaje) {
-    let numero;
-    let intentos = 0;
-
-    while (true) { 
-      let a = prompt(mensaje);
-        numero = parseFloat(a);
-        intentos++;
-
-        if (!isNaN(numero)) {
-            return numero; 
-        } else {
-            if (intentos >= 3) {
-                mensaje = "¿eres gilipollas?"; 
-
-            }
-        }
-    }
-}
-
-let n1 = pedirNumero("Escriba n1");
-let n2 = pedirNumero("Escriba n2");
-let n3 = pedirNumero("Escriba n3");
-let n4 = pedirNumero("Escriba n4");
-
-let valor = prompt("Que quieres hacer?, 1:suma, 2:resta, 3:multi, 4:divi");
-let valor2 = parseInt(valor);
-
-switch (valor2) {
-    case 1:
-        let total = n1+n2+n3+n4;
-        alert ("su suma es: "+ total);
-        break;
-    case 2:
-        let total2 = n1-n2-n3-n4;
-        alert ("su resta es: "+ total2);
-        break;
+let persona = {
+    nombre: "Juan",
+    edad: 30,
+    ciudad: "Madrid",
     
-    case 3:
-        let total3 = n1*n2*n3*n4;
-        alert ("su multiplicacion es: "+ total3);
-        break;
-    case 4:
-        let total4 = (n1 + n2 + n3 + n4) / 4;
-        alert ("su division es: "+ total4);
-        break;    
+    saludar: function() {
+        console.log("Hola, me llamo " + this.nombre);
+    }
+};
 
-    default:
-        alert("Opcion no valida");
-        break;
-}
+let persona2 = {
+    nombre: "Juan",
+    edad: 24,
+    ciudad: "Madrid"
+};
+
+
+let sonIguales = JSON.stringify(persona) === JSON.stringify(persona2);
+console.log(sonIguales); // true o false
--- calculadora/js/js.js
+++ calculadora/js/js.js
@@ -1,1 +1,48 @@
+function ponerNumero(numero){
+    let pantalla = document.getElementById('text');
+    pantalla.value += numero;
+}
+
+
+function ponerOperador(operador) {
+    let pantalla = document.getElementById('text');
+    pantalla.value += operador; 
+}
+
+function borrar(){
+    let pantalla = document.getElementById('text');
+    pantalla.value = '';
+}
+
+function calcular(){
+    let pantalla = document.getElementById('text');
+    if (pantalla.value !== "") {
+        pantalla.value = eval(pantalla.value); 
+    }
+}
+
+function elevar(){
+    let pantalla = document.getElementById('text');
+    let num = parseFloat(pantalla.value);
+    pantalla.value = num*num;
+}
+
+document.querySelectorAll('button').forEach(boton =>{
+    boton.onmouseover = () => {
+        let valor = boton.textContent.trim();
+
+    if (isNaN(valor) || valor === "") {
+      boton.style.backgroundColor = "yellow";
+    } 
+    else if (valor % 2 === 0) {
+      boton.style.backgroundColor = "red";
+    } 
+    else {
+      boton.style.backgroundColor = "green";
+    }
+  };
+  boton.onmouseout = () => {
+    boton.style.backgroundColor = "";
+  };
+});
 
